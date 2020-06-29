f6b2e0be3b5837e20747a1eab1cdbf425326a56b
Gabriela Mauricio <gabismauricio@gmail.com>
2020-06-29 12:35:50 -0300

    🎉 new swiftui project

---
+++ dotdos/dotdos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dotdos
+//
+//  Created by Gabriela Schirmer Mauricio on 28/06/20.
+//  Copyright © 2020 Gabriela Schirmer Mauricio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
